f101ab85853cf795cc7c425d1a15dd6225f2b6b7
CouchMeka <meka.daniels1@gmail.com>
2023-06-11 12:52:17 -0400

    updates

--- usereducerapp/src/App.js
+++ usereducerapp/src/App.js
@@ -4,67 +4,89 @@ import 'bootstrap/dist/css/bootstrap.css'
 import { Card, Row, Col } from 'react-bootstrap';
 
 // Action Types
-const SET_POSTS = "SET_POSTS";
+const SET_ITEMS = "SET_ITEMS";
 
 // Reducer
-const postsReducer = (state, action) => {
+const itemsReducer = (state, action) => {
   switch (action.type) {
-    case SET_POSTS:
-      return action.posts;
+    case SET_ITEMS:
+      return action.items;
     default:
       return state;
   }
 };
 
 function App() {
-  const [state, dispatch] = useReducer(postsReducer, null);
-  const [postCount, setPostCount] = useState(5);
+  const [state, dispatch] = useReducer(itemsReducer, null);
+  const [itemCount, setItemCount] = useState(5);
+  const [itemType, setItemType] = useState('posts');
 
   useEffect(() => {
-    const fetchPosts = async () => {
-      const response = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+    const fetchItems = async () => {
+      const response = await fetch(`https://jsonplaceholder.typicode.com/${itemType}`);
 
       if (!response.ok) {
         console.error(`API request failed with status ${response.status}`);
         return;
       }
 
-      let posts = await response.json();
+      let items = await response.json();
 
-      if (!Array.isArray(posts)) {
+      if (!Array.isArray(items)) {
         console.error('API response is not an array');
         return;
       }
 
-      // Slice the array of posts to desired length
-      posts = posts.slice(0, postCount);
+      // Slice the array of items to desired length
+      items = items.slice(0, itemCount);
 
-      dispatch({ type: SET_POSTS, posts });
+      dispatch({ type: SET_ITEMS, items });
     };
 
-    fetchPosts();
-  }, [postCount]);
+    fetchItems();
+  }, [itemCount, itemType]);
+
+  const renderCardText = (item) => {
+    switch(itemType) {
+      case 'posts':
+        return item.body;
+      case 'todos':
+        return `Completed: ${item.completed}`;
+      case 'users':
+        return `Email: ${item.email}`;
+      default:
+        return '';
+    }
+  }
 
   return (
     <div className="App">
-      <h1>Posts</h1>
+      <h1>{itemType.charAt(0).toUpperCase() + itemType.slice(1)}</h1>
       <label>
-        Number of posts to display:
+        Number of items to display:
         <input
           type="number"
-          value={postCount}
-          onChange={(e) => setPostCount(e.target.value)}
+          value={itemCount}
+          onChange={(e) => setItemCount(e.target.value)}
         />
+      </label>
+      <label>
+        Select type:
+        <select value={itemType} onChange={(e) => setItemType(e.target.value)}>
+          <option value="posts">Posts</option>
+          <option value="todos">Todos</option>
+          <option value="users">Users</option>
+        </select>
       </label>
       {state === null ? <p>Loading...</p> :
         <Row>
-          {state.map((post, index) => (
+          {state.map((item, index) => (
             <Col sm={4} key={index}>
               <Card style={{ width: '18rem', marginTop: '1rem' }}>
                 <Card.Body>
-                  <Card.Title>{post.title}</Card.Title>
+                  <Card.Title>{item.title || item.name}</Card.Title>
                   <Card.Text>
-                    {post.body}
+                    {renderCardText(item)}
                   </Card.Text>
                 </Card.Body>
               </Card>
@@ -77,4 +99,3 @@ function App() {
 }
 
 export default App;
-
